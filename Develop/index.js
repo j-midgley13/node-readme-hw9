1fddf97c36e4a0293283664155b8b6795b83118c
Jameson Midgley <jameson.midgley@gmail.com>
2020-07-21 23:22:53 -0600

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,63 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 // array of questions for user
-const questions = [
+// const questions = [
 
-];
+// ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+// function writeToFile(fileName, data) {
+// }
 
 // function to initialize program
 function init() {
-
-}
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter the title of your project.",
+            name: "title"
+        },
+        {
+            type: "input",
+            message: "Enter a description for your project.",
+            name: "description"
+        },
+        {
+            type: "input",
+            message: "Enter instructions on installation of your project.",
+            name: "installation"
+        },
+        {
+            type: "input",
+            message: "Enter instructions on how to use your project.",
+            name: "usage"
+        },
+        {
+            type: "input",
+            message: "Enter the contribution guidelines for your project.",
+            name: "contribution"
+        },
+        {
+            type: "input",
+            message: "Enter the test instructions for your project.",
+            name: "test"
+        },
+        {
+            type: "input",
+            message: "Enter your GitHub username.",
+            name: "username"
+        },
+        {
+            type: "input",
+            message: "Enter your email address.",
+            name: "email"
+        },
+    ])
+    .then(function(answers) {
+        console.log(answers);
+    })
+};
 
 // function call to initialize program
 init();
