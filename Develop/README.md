86751627040660fa109184a2e563833ecd3e1b88
Jameson Midgley <jameson.midgley@gmail.com>
2020-07-22 20:32:57 -0600

    Bones done for markdown

---
+++ Develop/README.md
@@ -0,0 +1,34 @@
+# Hello!
+
+## Description
+-------------
+This is my project
+
+## Table of Contents
+-------------
+
+## Installation
+-------------
+Use GitHub okay
+
+## Usage
+-------------
+It is pretty intuitive. Just give it a go.
+
+## License
+-------------
+This project is currently licensed under the MIT license.
+
+## Contributing
+-------------
+No contributions at this time. Thank you.
+
+## Tests
+-------------
+undefined
+
+## Questions
+-------------
+Any questions regarding this project please contact me at:
+GitHub: j-midgley13
+email: jameson.midgley@gmail.com
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,6 +2,38 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## Description
+-------------
+${data.description}
+
+## Table of Contents
+-------------
+
+## Installation
+-------------
+${data.installation}
+
+## Usage
+-------------
+${data.usage}
+
+## License
+-------------
+This project is currently licensed under the ${data.license} license.
+
+## Contributing
+-------------
+${data.contribution}
+
+## Tests
+-------------
+${data.test}
+
+## Questions
+-------------
+Any questions regarding this project please contact me at:
+GitHub: ${data.username}
+email: ${data.email}
 `;
 }
 
